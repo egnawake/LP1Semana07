7b4fb32494a6ccfb1a5704b0a3dacfe5fe6883c1
João Inácio <egnawake@gmail.com>
2022-04-04 23:25:51 +0100

    Instantiate some balls and call their methods

--- ColorBalls/Program.cs
+++ ColorBalls/Program.cs
@@ -6,8 +6,24 @@ namespace ColorBalls
     {
         private static void Main(string[] args)
         {
-            Color c = new Color(10, 20, 10, 255);
-            Console.WriteLine($"{c.GetGrayLevel()}");
+            // create colors
+            Color red = new Color(255, 0, 0);
+            Color yellow = new Color(255, 255, 0);
+            Color cyan = new Color(0, 255, 255);
+
+            // create balls
+            Ball ballOne = new Ball(red, 5);
+            Ball ballTwo = new Ball(yellow, 10);
+            Ball ballThree = new Ball(cyan, 20);
+
+            ballTwo.Throw();
+            ballTwo.Throw();
+
+            ballOne.Throw();
+
+            ballThree.Throw();
+            ballThree.Pop();
+            ballThree.Throw();
         }
     }
 }
