818efdce27730c650d745588c32d9a206d9dc87e
João Inácio <egnawake@gmail.com>
2022-04-06 22:09:30 +0100

    Add default value to alpha property

--- ColorBallsWithProperties/Color.cs
+++ ColorBallsWithProperties/Color.cs
@@ -5,7 +5,7 @@ namespace ColorBallsWithProperties
         public byte R { get; set; }
         public byte G { get; set; }
         public byte B { get; set; }
-        public byte A { get; set; }
+        public byte A { get; set; } = 255;
 
         public byte GrayLevel => (byte) ((R + G + B) / 3);
 
