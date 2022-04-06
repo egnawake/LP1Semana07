c22b60199f12ff0c1261cf8c1623cf430cbb7511
João Inácio <egnawake@gmail.com>
2022-04-06 22:05:41 +0100

    Add Color class with properties

---
+++ ColorBallsWithProperties/Color.cs
@@ -0,0 +1,14 @@
+namespace ColorBallsWithProperties
+{
+    public class Color
+    {
+        public byte R { get; set; }
+        public byte G { get; set; }
+        public byte B { get; set; }
+        public byte A { get; set; }
+
+        public byte GrayLevel => (byte) ((R + G + B) / 3);
+
+        public string AsString => $"({R}, {G}, {B}, {A})";
+    }
+}
