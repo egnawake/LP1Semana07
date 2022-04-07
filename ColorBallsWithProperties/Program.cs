7b00a9dcc625b68a92a74aeffbf3e5fbcb812439
João Inácio <egnawake@gmail.com>
2022-04-07 12:02:39 +0100

    Test Color.Name

--- ColorBallsWithProperties/Program.cs
+++ ColorBallsWithProperties/Program.cs
@@ -7,13 +7,17 @@ namespace ColorBallsWithProperties
         static void Main(string[] args)
         {
             Color green = new Color { R = 0, G = 255, B = 0 };
+            Color darkPurple = new Color { R = 127, G = 0, B = 127 };
             Console.WriteLine(green.AsString);
 
             Ball b1 = new Ball(green, 25);
             Console.WriteLine("{0}, {1}, {2}",
-                g.Color.AsString,
-                g.Radius,
-                g.TimesThrown);
+                b1.Color.AsString,
+                b1.Radius,
+                b1.TimesThrown);
+
+            Console.WriteLine($"{green.Name}");
+            Console.WriteLine($"{darkPurple.Name}");
         }
     }
 }
