e932edc53ab0e401c9e4e3523477adf161ae3368
João Inácio <egnawake@gmail.com>
2022-04-07 12:01:31 +0100

    Add Name property to Color class

--- ColorBallsWithProperties/Color.cs
+++ ColorBallsWithProperties/Color.cs
@@ -10,5 +10,13 @@ namespace ColorBallsWithProperties
         public byte GrayLevel => (byte) ((R + G + B) / 3);
 
         public string AsString => $"({R}, {G}, {B}, {A})";
+
+        public string Name => this switch
+        {
+            { R: 255, G: 0, B: 0 } => "Red 100%",
+            { R: 0, G: 255, B: 0 } => "Green 100%",
+            { R: 0, G: 0, B: 255 } => "Blue 100%",
+            _ => "Mixed"
+        };
     }
 }
